6ae8adf5c00f1528568d3bf6398d9026341f3545
ftp._balzsam <77864903+undefined024@users.noreply.github.com>
2021-07-05 20:48:51 +0200

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+from src import pypong
+
+def main():
+    game = pypong.PyPong()
+    player_one = game.PlayerOne(input("What is the first player name: "))
+    player_two = game.PlayerTwo(input("What is the second player name: "))
+    game.play(player_one, player_two)
+
+if __name__ == "__main__":
+    main()
